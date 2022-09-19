7d5660954ab003c73998523e1037a8d9c60b307b
codeDragon491 <trefasjulia@gmail.com>
2022-09-19 14:29:52 +0200

    move commit to action & clear abilities as well

--- src/composables/usePokemons.js
+++ src/composables/usePokemons.js
@@ -7,37 +7,35 @@ const loading = ref(false)
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const getPokemon = async (name) => {
-    error.value = null
-    loading.value = true
-    store.commit('pokemons/clearPokemon')
-    try {
-      const pokemons = JSON.parse(localStorage.getItem('pokemons'))
-      if(pokemons) {
-        const pokemonIsLoaded  = pokemons.some(pokemon => pokemon.name === name)
-        if (pokemonIsLoaded) {
-          store.commit('pokemons/getPokemon', name)
-        } else {
-          await store.dispatch('pokemons/getPokemon', name)
-          store.commit('pokemons/getPokemon', name)
-        }
+  error.value = null
+  loading.value = true
+  store.commit('pokemons/clearPokemon')
+  try {
+    const pokemons = JSON.parse(localStorage.getItem('pokemons'))
+    if (pokemons) {
+      const pokemonIsLoaded = pokemons.some(pokemon => pokemon.name === name)
+      if (pokemonIsLoaded) {
+        store.commit('pokemons/getPokemon', name)
       } else {
         await store.dispatch('pokemons/getPokemon', name)
-        store.commit('pokemons/getPokemon', name)
       }
-      await sleep(1000)
-      loading.value = false
-    } catch (err) {
-      if(err.response && err.response.data) {
-        error.value = err.response.data
-      } else error.value = 'Something went wrong. Try again later'
-      await sleep(1000)
-      loading.value = false
+    } else {
+      await store.dispatch('pokemons/getPokemon', name)
     }
+    await sleep(1000)
+    loading.value = false
+  } catch (err) {
+    if (err.response && err.response.data) {
+      error.value = err.response.data
+    } else error.value = 'Something went wrong. Try again later'
+    await sleep(1000)
+    loading.value = false
   }
+}
 
-  const pokemon = computed(() => store.state.pokemons.pokemon)
+const pokemon = computed(() => store.state.pokemons.pokemon)
 
-  const pokemonAbilities = computed(() => store.state.pokemons.abilities)
+const pokemonAbilities = computed(() => store.state.pokemons.abilities)
 
 
 const usePokemons = () => ({
--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ const pokemons = {
   mutations: {
     addPokemon(state, pokemon) {
       const pokemons = JSON.parse(localStorage.getItem('pokemons'))
-      if(pokemons){
+      if (pokemons) {
         state.list = pokemons
       }
       state.list.unshift(pokemon)
@@ -19,23 +19,25 @@ const pokemons = {
     },
     getPokemon(state, name) {
       const pokemons = JSON.parse(localStorage.getItem('pokemons'))
-      if(pokemons){
+      if (pokemons) {
         state.pokemon = pokemons.find(pokemon => pokemon.name === name)
       } else state.pokemon = state.list.find(pokemon => pokemon.name === name)
       state.abilities = [
-        {title:'abilities', abilities: state.pokemon.abilities, key: 'ability'}, 
-        {title:'moves', abilities: state.pokemon.moves, key: 'move'},
-        {title:'stats', abilities: state.pokemon.stats, key: 'stat'}
+        { title: 'abilities', abilities: state.pokemon.abilities, key: 'ability' },
+        { title: 'moves', abilities: state.pokemon.moves, key: 'move' },
+        { title: 'stats', abilities: state.pokemon.stats, key: 'stat' }
       ]
     },
     clearPokemon(state) {
       state.pokemon = null
+      state.abilities = []
     },
   },
   actions: {
-    async getPokemon({commit}, payload) {
+    async getPokemon({ commit }, payload) {
       const { data } = await app().get(payload)
       commit('addPokemon', data)
+      commit('getPokemon', payload)
     }
   },
 }
