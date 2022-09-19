7bb28b9f3cc9ba20fbd8098f13b641c44b3aea62
codeDragon491 <trefasjulia@gmail.com>
2022-09-19 16:42:32 +0200

    reuse pikachu string literal

--- src/views/TabbedView.vue
+++ src/views/TabbedView.vue
@@ -1,54 +1,80 @@
 <template>
   <div id="tabbed-page" class="container">
     <section class="tabs">
-      <tab v-for="tab in tabs" :key="tab.name" :tab="tab" @tab-clicked="getTabItem(tab)" 
+      <tab
+        v-for="tab in tabs"
+        :key="tab.name"
+        :tab="tab"
+        @tab-clicked="getTabItem(tab)"
         v-model:activeTab="activeTab"
       />
     </section>
     <section v-if="!loading" class="tab-item">
-      <tab-item :item="pokemon" :columns="pokemonAbilities" v-if="pokemon"/>
+      <tab-item :item="pokemon" :columns="pokemonAbilities" v-if="pokemon" />
       <div v-else-if="error">
         <p class="error">{{ error }}</p>
       </div>
     </section>
     <section v-else class="loader-wrapper">
-      <loader-round/>
+      <loader-round />
     </section>
   </div>
-
 </template>
 <script>
-import { ref, onBeforeMount, defineAsyncComponent } from "vue"
-import usePokemons from "@/composables/usePokemons"
-import Tab from '@/components/shared/Tab.vue';
+import { ref, onBeforeMount, defineAsyncComponent } from "vue";
+import usePokemons from "@/composables/usePokemons";
+import Tab from "@/components/shared/Tab.vue";
 
 export default {
   name: "TabbedView",
   components: {
     Tab,
-    TabItem: defineAsyncComponent(() =>  import('@/components/shared/TabItem.vue')),
-    LoaderRound: defineAsyncComponent(() =>  import('@/components/base/LoaderRound.vue'))
+    TabItem: defineAsyncComponent(() =>
+      import("@/components/shared/TabItem.vue")
+    ),
+    LoaderRound: defineAsyncComponent(() =>
+      import("@/components/base/LoaderRound.vue")
+    ),
   },
   setup() {
-  
-    const tabs = ref([ {name:'charmander', imageSrc: require('@/assets/images/charmander.jpg')} , {name:'pikachu', imageSrc: require('@/assets/images/pikachu.jpg')}, 
-    {name:'ditto', imageSrc: require('@/assets/images/ditto.png')}])
+    const middlePokemon = "pikachu";
+    
+    const tabs = ref([
+      {
+        name: "charmander",
+        imageSrc: require("@/assets/images/charmander.jpg"),
+      },
+      {
+        name: middlePokemon,
+        imageSrc: require("@/assets/images/pikachu.jpg"),
+      },
+      { name: "ditto", imageSrc: require("@/assets/images/ditto.png") },
+    ]);
 
-    const activeTab = ref('pikachu')
+    const activeTab = ref(middlePokemon);
 
-    const {loading, pokemon, pokemonAbilities, error, getPokemon} = usePokemons()
+    const { loading, pokemon, pokemonAbilities, error, getPokemon } =
+      usePokemons();
 
-    onBeforeMount(async() => {
-      await getPokemon('pikachu')
-    })
+    onBeforeMount(async () => {
+      await getPokemon("pikachu");
+    });
 
-    const getTabItem = async(tab) => {
-      activeTab.value = tab.name
-      await getPokemon(tab.name)
-    }
-    return { tabs, pokemon, loading, error, getTabItem, activeTab, pokemonAbilities}
-  }
-}
+    const getTabItem = async (tab) => {
+      activeTab.value = tab.name;
+      await getPokemon(tab.name);
+    };
+    return {
+      tabs,
+      pokemon,
+      loading,
+      error,
+      getTabItem,
+      activeTab,
+      pokemonAbilities,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
